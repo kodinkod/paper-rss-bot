1e11b2a459eb521f411502507feddb6dafba8447
kodidi <koddinrus@gmail.com>
2024-10-12 20:16:47 +0300

    fix

--- test.py
+++
@@ -1,7 +0,0 @@
-from langchain_openai import ChatOpenAI
-
-model = ChatOpenAI(model_name="gpt-3.5-turbo-0125",
-                   openai_api_key="sk-FyQrmncG5JvDAxO48JoYprNPES4omOdP",
-                   base_url= "https://api.proxyapi.ru/openai/v1"
-                   )
-print(model.invoke("What is the capital of France?").content)
